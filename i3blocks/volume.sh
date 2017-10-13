6d981c6103b447207ae4387b34a44ddd463354b5
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-14 01:24:40 +0530

    [humble_D] Volume scipt added

---
+++ i3blocks/volume.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# picking up last line a amixer output
+commandOutput=$(amixer -c 0 get Master | tail -n1);
+
+# extracting volume percentatage
+volumeValue=$(echo $commandOutput | cut -d ' ' -f 4 | sed 's/[][]//g');
+
+# extracting mute status
+isMute=$(echo $commandOutput | cut -d ' ' -f 6 | sed 's/[][]//g');
+
+if [ "$isMute" = "off" ]
+then
+    # full name
+    echo " $volumeValue";
+    
+    # short name
+    echo " $volumeValue";
+    
+    # color
+    echo "#FF8A80"
+else
+    # full name
+    echo " $volumeValue";
+    
+    # short name
+    echo " $volumeValue";
+    
+    # color
+    echo "#A7FFEB"
+fi
