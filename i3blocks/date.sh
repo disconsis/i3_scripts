eab4777cebd7f49a2e1095cfcb6bd78ccfe214b8
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-15 17:12:35 +0530

    [humble_D] commenting in date script

--- i3blocks/date.sh
+++ i3blocks/date.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo `date '+%a %d %b %Y'`;
+# using date command formatting to extract date in the format of <Week Day, day of month, month name, year>
+date=$(date '+%a %d %b %Y')
+
+# full name
+echo $date;
+
+# short name
+echo $date;
