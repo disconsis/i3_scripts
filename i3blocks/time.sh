bca87843a6cf0dde8f977939280cf8f6d72ba535
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-15 17:12:45 +0530

    [humble_D] commenting in time script

--- i3blocks/time.sh
+++ i3blocks/time.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo `date '+%I:%M %p'`;
+# using date command formatting to extract time in the format of <Hour, Minute, AM/PM>
+time=$(date '+%I:%M %p')
+
+# full name
+echo $time;
+
+# short name
+echo $time;
