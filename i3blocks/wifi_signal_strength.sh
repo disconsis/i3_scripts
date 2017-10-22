72118e910a6e6773349e8ae20d114ee43b59ad1a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-23 04:33:08 +0530

    [humble_D] Highly customizable wifi info script ready

--- i3blocks/wifi_signal_strength.sh
+++ i3blocks/wifi_signal_strength.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# output for wireless interface
-if [[ $# -ge 1 ]]; then
-    iface=$1
+# default variable values
+offColor="#ffffff";
+ssidColor="#ffffff";
+signalColor="#ffffff";
+innerSeparatorColor="#ffffff";
+
+# output for wireless interface ( if ssidc sigc offc insc )
+if [[ $# -ge 5 ]]; then
+    iface=$1;
+    ssidColor=$2;
+    signalColor=$3;
+    offColor=$4;
+    innerSeparatorColor=$5;
+elif [[ $# -eq 4 ]]; then
+    iface=$1;
+    ssidColor=$2;
+    signalColor=$3;
+    offColor=$4;
+elif [[ $# -eq 3 ]]; then
+    iface=$1;
+    ssidColor=$2;
+    signalColor=$3;
+elif [[ $# -eq 2 ]]; then
+    iface=$1;
+    ssidColor=$2;
+elif [[ $# -eq 1 ]]; then
+    iface=$1;
 else
-    iface="wlp58s0"
+    echo "iface needed. ERROR!!"
+    exit;
 fi
 
 iwconfigOutput=$(iwconfig $iface);
@@ -13,11 +38,9 @@ flag=$(echo $iwconfigOutput | grep "ESSID" | cut -d ':' -f2);
 
 if [ "$flag" = "off/any  " ]; then
     # fullname
-    echo " N/A   N/A";
+    echo "<span foreground='$offColor'> N/A</span> <span foreground='$innerSeparatorColor'> ∙ </span> <span foreground='$offColor'> N/A</span>";
     # shortname
-    echo " N/A   N/A";
-    # color
-    echo "#FF80AB";
+    echo "<span foreground='$offColor'> N/A</span> <span foreground='$innerSeparatorColor'> ∙ </span> <span foreground='$offColor'> N/A</span>";
 else
     # extracting the ssid of the connection
     ssid=$( echo "$iwconfigOutput" | grep "ESSID" | cut -d '"' -f2);
@@ -27,9 +50,7 @@ else
     signalStrengthPercentage=$( echo "$signalStrength"*100 | bc | awk '{print int($1)}');
     
     # fullname
-    echo " $ssid   $signalStrengthPercentage%";
+    echo "<span foreground='$ssidColor'> $ssid</span> <span foreground='$innerSeparatorColor'> ∙ </span> <span foreground='$signalColor'> $signalStrengthPercentage%</span>";
     # shortname
-    echo " $ssid   $signalStrengthPercentage%";
-    # color
-    echo "#18FFFF";
+    echo "<span foreground='$ssidColor'> $ssid</span> <span foreground='$innerSeparatorColor'> ∙ </span> <span foreground='$signalColor'> $signalStrengthPercentage%</span>";
 fi
