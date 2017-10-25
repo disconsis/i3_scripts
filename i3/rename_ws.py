f1c60606247bb64242e6d663ae6e4e5f63e3c112
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2017-10-25 15:08:08 +0530

    [disconsis] made rename_ws.py more robust

--- i3/rename_ws.py
+++ i3/rename_ws.py
@@ -10,55 +10,68 @@ import argparse
 LOCK_FILE = '/tmp/ws_name_lock'
 
 
+def escape(name):
+    name = '\\"'.join(name.split('"'))
+    return name
+
+
 @fasteners.interprocess_locked(LOCK_FILE)
 def get_new_name(i3, input_name):
+    input_name = escape(input_name)
     workspace = i3.get_tree().find_focused().workspace()
     count = workspace.name.count(':')
-    # unnamed workspace
     if count in {0, 1}:
+        # unnamed workspace
         apps = workspace.name.split(':')[1]
-    # named workspace
-    elif count == 2:
-        apps = workspace.name.split(':')[2]
     else:
-        raise ValueError
+        # named workspace
+        apps = workspace.name.split(':')[-1]
 
     new_name = "{}: {}:{}".format(workspace.num, input_name, apps)
+    print(new_name)
     return new_name, workspace
 
 
 def rename(i3, args):
-    try:
-        new_name, workspace = get_new_name(i3, args.name)
-    except ValueError:
-        proc.call(['i3-nagbar', '-m',
-                   '"too many `:` in workspace {}"'.format(workspace.num)])
-        return
-
+    new_name, workspace = get_new_name(i3, ' '.join(args.name))
     if new_name != workspace.name:
-        workspace.command('rename workspace "{}" to "{}"'.format(workspace.name,
-                                                                 new_name))
+        print('rename workspace to "{}"'.format(new_name))
+        workspace.command('rename workspace to "{}"'.format(new_name))
 
 
 @fasteners.interprocess_locked(LOCK_FILE)
 def remove(i3, args):
     workspace = i3.get_tree().find_focused().workspace()
-    assert workspace.name.count(':') == 2, "invalid workspace name structure"
+    assert workspace.name.count(':') >= 2, "invalid workspace name structure"
     split_name = workspace.name.split(':')
-    new_name = ':'.join((split_name[0], split_name[2]))
-    workspace.command('rename workspace "{}" to "{}"'.format(workspace.name,
-                                                             new_name))
+    new_name = ':'.join((split_name[0], split_name[-1]))
+    workspace.command('rename workspace to "{}"'.format(new_name))
 
 
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     rename_parser = subparsers.add_parser('rename')
-    rename_parser.add_argument('name')
+    rename_parser.add_argument('name', nargs='+')
     rename_parser.set_defaults(func=rename)
     remove_parser = subparsers.add_parser('remove')
     remove_parser.set_defaults(func=remove)
     args = parser.parse_args()
+    with open('/tmp/blah', 'a') as fp:
+        fp.write(str(args) + '\n')
+    i3 = i3ipc.Connection()
+    args.func(i3, args)
+
+
+def testmain(string):
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    rename_parser = subparsers.add_parser('rename')
+    rename_parser.add_argument('name', nargs='+')
+    rename_parser.set_defaults(func=rename)
+    remove_parser = subparsers.add_parser('remove')
+    remove_parser.set_defaults(func=remove)
+    args = parser.parse_args(string.split())
     i3 = i3ipc.Connection()
     args.func(i3, args)
 
